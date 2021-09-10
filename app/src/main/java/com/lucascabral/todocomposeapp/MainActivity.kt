d8a3b4d9f99a0bdd3cdb512955612bec211db771
Lucas Cabral <lucas_cabral.deve@outlook.com>
2021-09-10 17:11:16 -0300

    Init commit

---
+++ app/src/main/java/com/lucascabral/todocomposeapp/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.lucascabral.todocomposeapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.lucascabral.todocomposeapp.ui.theme.ToDoComposeAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ToDoComposeAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ToDoComposeAppTheme {
+        Greeting("Android")
+    }
+}
