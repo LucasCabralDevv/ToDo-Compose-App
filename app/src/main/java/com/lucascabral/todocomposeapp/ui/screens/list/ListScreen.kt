c2bede18037334f903506d4652a4032253f0af8e
Lucas Cabral <lucas_cabral.deve@outlook.com>
2021-10-06 19:28:07 -0300

    Delete all tasks message has been modified.

--- app/src/main/java/com/lucascabral/todocomposeapp/ui/screens/list/ListScreen.kt
+++ app/src/main/java/com/lucascabral/todocomposeapp/ui/screens/list/ListScreen.kt
@@ -94,7 +94,7 @@ fun DisplaySnackBar(
         if (action != Action.NO_ACTION) {
             scope.launch {
                 val snackBarResult = scaffoldState.snackbarHostState.showSnackbar(
-                    message = "${action.name}: $taskTitle",
+                    message = setMessage(action = action, taskTitle = taskTitle),
                     actionLabel = setActionLabel(action)
                 )
                 undoDeleteTask(
@@ -104,6 +104,13 @@ fun DisplaySnackBar(
                 )
             }
         }
+    }
+}
+
+private fun setMessage(action: Action, taskTitle: String): String {
+    return when (action) {
+        Action.DELETE_ALL -> "All Tasks Removed."
+        else -> "${action.name}: $taskTitle"
     }
 }
 
