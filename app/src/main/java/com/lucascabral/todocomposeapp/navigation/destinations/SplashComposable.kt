2d2c05992d27d8f9118688586deb039cc179b41c
Lucas Cabral <lucas_cabral.deve@outlook.com>
2021-11-18 18:32:41 -0300

    CompileSdk configurado para v 31.

--- app/src/main/java/com/lucascabral/todocomposeapp/navigation/destinations/SplashComposable.kt
+++ app/src/main/java/com/lucascabral/todocomposeapp/navigation/destinations/SplashComposable.kt
@@ -1,6 +1,8 @@
 package com.lucascabral.todocomposeapp.navigation.destinations
 
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.slideOutVertically
 import androidx.navigation.NavGraphBuilder
 import com.google.accompanist.navigation.animation.composable
 import com.lucascabral.todocomposeapp.ui.screens.splash.SplashScreen
@@ -11,7 +13,12 @@ fun NavGraphBuilder.splashComposable(
     navigateToListScreen: () -> Unit
 ) {
     composable(
-        route = SPLASH_SCREEN
+        route = SPLASH_SCREEN,
+        exitTransition = { _, _ ->
+            slideOutVertically(
+                animationSpec = tween(300)
+            )
+        }
     ) {
         SplashScreen(
             navigateToListScreen = navigateToListScreen
